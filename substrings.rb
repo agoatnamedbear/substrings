4936656d14543581d1a2ad4a842fc0f900d7725f
Sean Wildman <agoatnamedbear@gmail.com>
2024-07-12 15:16:13 -0400

    update code to use each_with_object instead of reduce

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,24 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 sentence = "Howdy partner, sit down! How's it going?"
 
-def substrings(string, dictionary)
-  arr = string.downcase.split(' ')
+# def substrings(string, dictionary)
+#   arr = string.downcase.split(' ')
 
-  dictionary.reduce(Hash.new(0))  {|hash, word|
-    arr.each {|item|
-      hash[word] += 1 if item.include?(word)
-    }
-    hash
-  }
+#   dictionary.reduce(Hash.new(0))  {|hash, word|
+#     arr.each {|item|
+#       hash[word] += 1 if item.include?(word)
+#     }
+#     hash
+#   }
+# end
+
+
+
+def substring(sentence, dictionary)
+  arr = sentence.downcase.split(' ')
+  dictionary.each_with_object(Hash.new(0)) do |word, hash|
+    arr.each {|item| hash[word] += 1 if item.include?(word) }
+  end
 end
 
-p substrings(sentence, dictionary)
+p substring(sentence, dictionary)
